1c7d5eef4f93a831d955541e48fcae5aa06cc4f7
MichalKotKawula <kotkawula@gmail.com>
2023-10-09 13:12:40 -0400

    changes to config files

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -4,6 +4,23 @@
  * The main entry-point for the v1 version of the fragments API.
  */
 const express = require('express');
+const contentType = require('content-type');
+
+//Assignment 1
+const Fragment = require('../../model/fragment');
+const rawBody = () =>
+  express.raw({
+    inflate: true,
+    limit: '5mb',
+    type: (req) => {
+      // See if we can parse this content type. If we can, `req.body` will be
+      // a Buffer (e.g., `Buffer.isBuffer(req.body) === true`). If not, `req.body`
+      // will be equal to an empty Object `{}` and `Buffer.isBuffer(req.body) === false`
+      const { type } = contentType.parse(req);
+      return Fragment.isSupportedType(type);
+    },
+  });
+// End of Assignment 1 modification
 
 // Create a router on which to mount our API endpoints
 const router = express.Router();
@@ -12,5 +29,6 @@ const router = express.Router();
 router.get('/fragments', require('./get'));
 
 // Other routes will go here later on...
+router.post('/fragments', rawBody(), require('./post'));
 
 module.exports = router;
