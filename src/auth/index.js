310de056e253a37e4bc8ba042b2331dbd832648a
MichalKotKawula <kotkawula@gmail.com>
2023-11-29 23:46:29 -0500

    2aads

--- src/auth/index.js
+++ src/auth/index.js
@@ -8,10 +8,10 @@ if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
 }
 // Also allow for an .htpasswd file to be used, but not in production
 // eslint-disable-next-line no-undef
-// else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
-//   module.exports = require('./basic-auth');
-//   logger.info(`[Authorization strategy] Basic Auth`);
-// }
+else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
+  module.exports = require('./basic-auth');
+  logger.info(`[Authorization strategy] Basic Auth`);
+}
 // In all other cases, we need to stop now and fix our config
 else {
   throw new Error('missing env vars: no authorization configuration found');
